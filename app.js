c64b46a4325c45dd033c41e4de10746b4c8adb4f
Glitch (messenger-platform-quick-start) <none>
2023-10-19 22:27:16 +0000

    👑👟 Checkpoint ./app.js:62021859/241 ./package.json:62021859/22

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.post("/webhook", (req, res) => {
   //console.log(JSON.stringify(req.body, null, 2));
   let ar = JSON.stringify(req.body, null, 2)
   let data = {
-    
+    phone_number_id: req.body.entry[0].changes[0].value.metadata.phone_number_id,
+    from: req.body.entry[0].changes[0].value.messages[0].from,
+    msg_body: req.body.entry[0].changes[0].value.messages[0].text.body
   }
   //console.log(req.body)
   axios.post('http://localhost:3000//static_resources/api/v1/whatsapp_datas/save', data, {
